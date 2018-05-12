4f7a0a529f3619068e206018ca4e3fce1ed518f8
Erik Wittkorn <erik.wittkorn@gmail.com>
2018-05-12 14:13:54 +0200

    level script

---
+++ aframe_level/script/components.js
@@ -0,0 +1,34 @@
+AFRAME.registerComponent('log', {
+  schema: {
+    type: 'string'
+  },
+
+  init: function () {
+    var stringToLog = this.data;
+    console.log(stringToLog);
+  }
+});
+
+AFRAME.registerComponent('do-something', {
+  init: function () {
+    var sceneEl = this.el;
+    console.log('do something ', sceneEl);
+
+    for (var x = -1; x < 2; x++)
+      for (var z = -1; z < 2; z++) {
+        if (x === 0 && z === 0)
+          continue;
+
+        var boxEl = document.createElement('a-box');
+        boxEl.setAttribute('position', {
+          x: x * 1.5,
+          y: 1,
+          z: z * 1.5
+        });
+        boxEl.setAttribute('color', 'blue');
+
+        sceneEl.appendChild(boxEl);
+      }
+
+  }
+});
